ffa100d0f9fa52e5f108d21cac7156424c26f2df
Jake Dudum <jakedudum@gmail.com>
2019-07-01 16:54:04 -0700

    Created temporay dummy friends array in friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+var friends = [
+    {
+        name: "filler1",
+        score: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "filler2",
+        score: [2,2,2,2,2,2,2,2,2,2]
+    },
+    {
+        name: "filler3",
+        score: [3,3,3,3,3,3,3,3,3,3]
+    },
+    {
+        name: "filler4",
+        score: [4,4,4,4,4,4,4,4,4,4]
+    },
+    {
+        name: "filler5",
+        score: [5,5,5,5,5,5,5,5,5,5]
+    },
+    {
+        name: "filler6",
+        score: [1,2,3,4,5,5,4,3,2,1]
+    },
+    {
+        name: "filler7",
+        score: [5,4,3,2,1,1,2,3,4,5]
+    },
+    {
+        name: "filler8",
+        score: [1,2,3,1,2,3,1,2,3,1]
+    },
+    {
+        name: "filler9",
+        score: [3,2,1,3,2,1,3,2,1,3]
+    },
+    {
+        name: "filler10",
+        score: [2,4,1,3,5,2,4,1,3,5]
+    }
+];
