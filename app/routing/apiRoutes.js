1019579ed4dcbe43bc64d7e91d382b2de542a310
Jake Dudum <jakedudum@gmail.com>
2019-07-01 20:20:12 -0700

    Filled in client-side javascript for survey.html and filled in comparision logic for apiRoutes.js to determine the best friend match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,27 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    // need friend logic
+
+    var scores = [];
+
+    for (var i = 0; i < friends.length; i++) {
+        var total = 0;
+        for( var j = 0; j < friends[i].score.length; j++) {
+            total += Math.abs(req.body.scores[j] - friends[i].score[j]);
+        }
+        scores.push(total);
+    }
+
+    var smlIndex = 0;
+    var smallest = scores[0];
+
+    for (var k = 1; k < scores.length; k++) {
+        if (scores[k] < smallest) {
+            smlIndex = k;
+            smallest = scores[k];
+        }
+    }
+
+    res.send(friends[smlIndex].name);
   });
 };
